722339c2dc64adf467775628e19e2018c7c35585
IM Jiyoung <hkkgjb468@gmail.com>
2022-08-18 16:22:37 +0900

    FIRSTCOMMIT

---
+++ TODYSTUDY/src/main/java/com/study/toystudy/TestController.java
@@ -0,0 +1,16 @@
+package com.study.toystudy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value="/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+	    return new ModelAndView("book/create");
+
+	}
+}
